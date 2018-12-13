7711312306b9ea497b39980be00ee6ba15195ca1
Зотьев Андрей Николаевич <Zotev_A@INCOM.RU>
2018-12-13 18:41:53 +0300

    some mark security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,12 +18,13 @@ class SecurityController extends AbstractController
 
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,'h1'=>'auth']);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'h1'=>'auth'
+        ]);
     }
 
     public function register(Request $request,UserPasswordEncoderInterface $passwordEncoder,GuardAuthenticatorHandler $guardHandler , LoginFormAuthenticator $authenticator){
@@ -31,7 +32,6 @@ class SecurityController extends AbstractController
         $form = $this->createForm(UserType::class,$user);
         $form->add('Submit',SubmitType::class);
         $form->handleRequest($request);
-
         if($form->isSubmitted() && $form->isValid()){
             $password = $passwordEncoder->encodePassword($user,$user->getPassword());
             $user->setPassword($password);
